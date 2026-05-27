513c3f8d769b349e4ee6ddb68f055eca7fc935bb
mackarovak <ksenija.mackarova2014@yandex.ru>
2026-05-27 19:35:41 +0300

    deploy-vps-server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["medcat-team1.213-165-209-28.nip.io"],
+    allowedHosts: ["medcat-team1.213-165-209-28.nip.io", "144.124.240.63"],
   },
 });
